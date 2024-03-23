9504649813b8f43a5f9cb752ec5f9e69975ab441
Oni <darkninja407@gmail.com>
2024-03-24 04:45:39 +0800

    new file:   HTML/Main/Homepage.html 	new file:   HTML/Main/JS/firebase.js 	new file:   HTML/Main/JS/login.js 	new file:   HTML/Main/JS/register.js 	modified:   HTML/Main/Login.html 	modified:   HTML/Main/Main/CSS/Login.css 	modified:   HTML/Main/Main/CSS/Register.css 	modified:   HTML/Main/Register.html 	deleted:    HTML/Main/TEST.HTML
    	modified:   HTML/Main/JS/login.js
    	modified:   HTML/Main/Login.html
    	modified:   HTML/Main/Register.html

---
+++ HTML/Main/JS/login.js
@@ -0,0 +1,38 @@
+var signup = () => {
+    window.location.assign("../Main/Register.html")
+}
+
+const  Username = document.getElementById("Username")
+const Password = document.getElementById("Password")
+const  Message =document.getElementById ("message")
+
+const login = ()=>{
+    if (Username.value===""){
+        Message.innerHTML ="Username is required"
+        Message.style.color= "red"
+
+    }else if (Password.value===""){
+        Message.innerHTML ="Password is required"
+        Message.style.color= "red"
+} else {
+    const userData = {
+        Username: Username.value,
+        Password: Password.value,
+    }
+    firebase.auth().signInWithEmailAndPassword(userData.Email, userData.Password)
+  .then((userCredential) => {
+    Message.innerHTML = "Sign in Sucessfully"
+    Message.style.color = "green"
+    if (userCredential.user.emailVerfied){
+        window.location.assign("../Main/Homepage.html")
+    
+    }
+   
+    
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+  });
+
+}
